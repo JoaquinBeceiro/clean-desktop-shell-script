54864b460554c80cfe7f67cf0d682615d5617068
JoaquinBeceiro <joaquinbeceiro@gmail.com>
2018-08-28 17:13:17 -0300

    first commit

---
+++ cleanDesktop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir ~/Documents/ScreenShots/ >/dev/null 2>&1
+mkdir ~/Documents/PDFs/ >/dev/null 2>&1
+mkdir ~/Documents/CSVs/ >/dev/null 2>&1
+mkdir ~/Documents/IMAGEs/ >/dev/null 2>&1
+
+mv ~/Desktop/*Screen\ Shot* ~/Documents/ScreenShots/ >/dev/null 2>&1
+mv ~/Desktop/*.pdf ~/Documents/PDFs/ >/dev/null 2>&1
+mv ~/Desktop/*.psd ~/Documents/PSDs/ >/dev/null 2>&1
+mv ~/Desktop/*.csv ~/Documents/CSVs/ >/dev/null 2>&1
+mv ~/Desktop/*.jpg ~/Documents/IMAGEs/ >/dev/null 2>&1
+mv ~/Desktop/*.svg ~/Documents/IMAGEs/ >/dev/null 2>&1
+mv ~/Desktop/*.webp ~/Documents/IMAGEs/ >/dev/null 2>&1
+mv ~/Desktop/*.png ~/Documents/IMAGEs/ >/dev/null 2>&1
+printf '\e[32m Desktop cleaned!\n'
