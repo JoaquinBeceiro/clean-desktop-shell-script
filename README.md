b8e0f815b1cccb361482e8268f35f4f1b877e3ec
JoaquinBeceiro <joaquinbeceiro@gmail.com>
2018-08-30 16:10:29 -0300

    - Add some extensions. -  Rewrite README text

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # clean-desktop-shell-script
+
+**With this script you can clean your junk desktop and put all files in Document/SOMETHING according to the file extention.**
+
+## Extension supported 
+1. MAC screen shots
+2. PDF documents
+3. PSD documents
+4. CSV files
+5. JPG, JPEG, SVG and PNG images
+6. GIF images
+7. FONT files
+
+## Usage
+- To use this script you only need to write to your terminal `sh ./cleanDesktop.sh` and you will recive the message `Desktop cleaned!`.
+
+## Authors
+ **Joaquin Beceiro** 
+- [GitHub](https://github.com/JoaquinBeceiro) 
+- [Web](https://JoaquinBeceiro.com.uy)
--- cleanDesktop.sh
+++ cleanDesktop.sh
@@ -2,15 +2,22 @@
 
 mkdir ~/Documents/ScreenShots/ >/dev/null 2>&1
 mkdir ~/Documents/PDFs/ >/dev/null 2>&1
+mkdir ~/Documents/PSDs/ >/dev/null 2>&1
 mkdir ~/Documents/CSVs/ >/dev/null 2>&1
 mkdir ~/Documents/IMAGEs/ >/dev/null 2>&1
+mkdir ~/Documents/GIFs/ >/dev/null 2>&1
+mkdir ~/Documents/FONTs/ >/dev/null 2>&1
 
 mv ~/Desktop/*Screen\ Shot* ~/Documents/ScreenShots/ >/dev/null 2>&1
 mv ~/Desktop/*.pdf ~/Documents/PDFs/ >/dev/null 2>&1
 mv ~/Desktop/*.psd ~/Documents/PSDs/ >/dev/null 2>&1
 mv ~/Desktop/*.csv ~/Documents/CSVs/ >/dev/null 2>&1
 mv ~/Desktop/*.jpg ~/Documents/IMAGEs/ >/dev/null 2>&1
+mv ~/Desktop/*.jpeg ~/Documents/IMAGEs/ >/dev/null 2>&1
 mv ~/Desktop/*.svg ~/Documents/IMAGEs/ >/dev/null 2>&1
 mv ~/Desktop/*.webp ~/Documents/IMAGEs/ >/dev/null 2>&1
 mv ~/Desktop/*.png ~/Documents/IMAGEs/ >/dev/null 2>&1
+mv ~/Desktop/*.gif ~/Documents/GIFs/ >/dev/null 2>&1
+mv ~/Desktop/*.ttf ~/Documents/FONTs/ >/dev/null 2>&1
+mv ~/Desktop/*.otf ~/Documents/FONTs/ >/dev/null 2>&1
 printf '\e[32m Desktop cleaned!\n'
